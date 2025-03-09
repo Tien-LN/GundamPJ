d6c83761e07d9e2a66f667830f9571963b0b4299
Le Viet Xuan <xuantryingbetter@gmail.com>
2025-03-10 03:58:42 +0700

    enrollments

--- backend/src/routes/api/enrollment.route.js
+++ backend/src/routes/api/enrollment.route.js
@@ -4,4 +4,18 @@ const controller = require("../../controllers/api/enrollment.controller");
 
 router.get("/", controller.index);
 
+router.post("/", controller.createPost);
+
+router.delete("/:id", controller.enrollDelete);
+
+router.get("/list/:id", controller.list);
+
+router.get("/listApproved/:id", controller.listApproved);
+
+router.patch("/reject/:id", controller.reject);
+
+router.patch("/approve/:id", controller.approve);
+
+
+
 module.exports = router;
