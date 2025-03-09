6911574b84f997a6f8f57091b541af4706c56c61
Tien Lam <nhattienlam2005@gmail.com>
2025-03-10 00:09:50 +0700

    now have two MD: verifyUser and verifyAdmin

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,26 +1,40 @@
 const jwt = require("jsonwebtoken");
 const { prisma } = require("../config/db.js");
 
-const isAdmin = async (req, res, next) => {
+const verifyUser = async (req, res, next) => {
   try {
-    const token = req.headers.authorization?.split(" ")[1];
+    const token = req.cookies.jwt;
     if (!token) {
-      return res.status(401).json({ message: "chưa đăng nhập" });
+      return res.status(401).json({ message: "Bạn chưa đăng nhập" });
     }
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     const user = await prisma.user.findUnique({ where: { id: decoded.id } });
 
-    if (!user || user.role != "ADMIN") {
-      return res
-        .status(403)
-        .json({ message: "bạn không có quyền thực hiện thao tác này" });
+    if (!user) {
+      return res.status(401).json({ message: "Người dùng không tồn tại" });
     }
 
-    req.user = user;
+    req.user = user; // Lưu thông tin user vào req
     next();
   } catch (error) {
-    res.status(401).json({ message: "token không hợp lệ", error });
+    res.status(401).json({ message: "Token không hợp lệ", error });
   }
 };
 
-module.exports = { isAdmin };
+const verifyAdmin = async (req, res, next) => {
+  try {
+    await verifyUser(req, res, async () => {
+      if (req.user.role !== "ADMIN") {
+        return res
+          .status(403)
+          .json({ message: "Bạn không có quyền thực hiện thao tác này" });
+      }
+      next();
+    });
+  } catch (error) {
+    res.status(403).json({ message: "Token không hợp lệ", error });
+  }
+};
+
+module.exports = { verifyUser, verifyAdmin };
