f23f75b44e1e156193c74c4cb992a373707dee74
Le Viet Xuan <xuantryingbetter@gmail.com>
2025-03-10 03:12:19 +0700

    users

--- backend/src/routes/api/user.route.js
+++ backend/src/routes/api/user.route.js
@@ -4,4 +4,14 @@ const controller = require("../../controllers/api/user.controller");
 
 router.get("/", controller.getUsers);
 
+router.post("/create", controller.createPost);
+
+router.delete("/:id", controller.userDelete);
+
+router.patch("/lock/:id", controller.userLock);
+
+router.patch("/unlock/:id", controller.userUnLock);
+
+router.patch("/:id", controller.userUpdate);
+
 module.exports = router;
