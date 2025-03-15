e63c8962512941f08ad134a24c0db7fcca8abf5c
dung <quangdung21102005@gmail.com>
2025-03-15 08:33:51 +0700

    add header

--- frontend/src/layouts/client/Header.jsx
+++ frontend/src/layouts/client/Header.jsx
@@ -1,7 +1,19 @@
-function Header(){
+import { NavLink } from 'react-router-dom';
+import './header.css'
+function Header() {
     return (
         <>
-            Header 
+            <div className="container">
+                <img className="logo" src="../public/img/logo.png" />
+                <nav className="nav-bar">
+                    <div className='nav-link'>
+                        <NavLink to="/home" className="nav-link-c">Trang chủ</NavLink>
+                        <NavLink to="/courses" className="nav-link-c">Khoá học của tôi</NavLink>
+                        <NavLink to="/statistics" className="nav-link-c">Thống kê</NavLink>
+                    </div>
+                    <img className="logo-account" src="../public/img/Account.png" />
+                </nav>
+            </div>
         </>
     )
 };
