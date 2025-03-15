64d236cb6a08259cb9b6f20a52d5f623d43a150b
dung <quangdung21102005@gmail.com>
2025-03-15 11:19:45 +0700

    add home page

--- frontend/src/layouts/client/Header.jsx
+++ frontend/src/layouts/client/Header.jsx
@@ -1,17 +1,18 @@
 import { NavLink } from 'react-router-dom';
 import './header.css'
+
 function Header() {
     return (
         <>
-            <div className="container">
+            <div className="header-container">
                 <img className="logo" src="../public/img/logo.png" />
                 <nav className="nav-bar">
                     <div className='nav-link'>
-                        <NavLink to="/home" className="nav-link-c">Trang chủ</NavLink>
+                        <NavLink to="/" className="nav-link-c">Trang chủ</NavLink>
                         <NavLink to="/courses" className="nav-link-c">Khoá học của tôi</NavLink>
                         <NavLink to="/statistics" className="nav-link-c">Thống kê</NavLink>
                     </div>
-                    <img className="logo-account" src="../public/img/Account.png" />
+                    <NavLink to="/my-account"><img className="logo-account" src="../public/img/Account.png" /></NavLink>
                 </nav>
             </div>
         </>
