4640deff207830f2c19ddbc5c172dd51349c2cbd
Tien Lam <nhattienlam2005@gmail.com>
2025-03-17 10:17:59 +0700

    update doc, enroll, exam controller

--- backend/src/controllers/api/exam.controller.js
+++ backend/src/controllers/api/exam.controller.js
@@ -1,271 +1,277 @@
 const { prisma } = require("../../config/db");
 
 // [GET] /api/exams/?courseId=:id
-module.exports.index = async(req, res) => {
-    try{
-        const courseId = req.query.courseId;
-        if(!courseId){
-            return res.status(400).json({message: "courseId is required!"});
-        }
-        const course = await prisma.course.findFirst({
-            where: {id: courseId},
-            include: {
-                exams: true
-            }
-        });
-        if(!course){
-            return res.status(400)
-        }
-        return res.status(200).json(course);
-    } catch(error){
-        return res.status(500).json({message: "Server error!", error});
+module.exports.index = async (req, res) => {
+  try {
+    const courseId = req.query.courseId;
+    if (!courseId) {
+      return res.status(400).json({ message: "courseId is required!" });
     }
-}
+    const course = await prisma.course.findFirst({
+      where: { id: courseId },
+      include: {
+        exams: true,
+      },
+    });
+    if (!course) {
+      return res.status(400);
+    }
+    return res.status(200).json(course);
+  } catch (error) {
+    return res.status(500).json({ message: "Server error!", error });
+  }
+};
 
-// [POST] /api/exams/create 
-module.exports.createPost = async(req, res) => {
-    if(req.body.questions && req.body.questions.length == 0) delete req.body.questions;
-    if(req.body.UserExam && req.body.UserExam.length == 0) delete req.body.UserExam;
-    const course = await prisma.course.findUnique({
-        where: {id: req.body.courseId}
-    });
-    if(!course){
-        return res.status(400).json({message: "Lỗi không tìm thấy khóa học"});
-    }
-    await prisma.exam.create({
-        data: req.body
-    });
-    res.send("Đã tạo bài thi thành công");
-} 
+// [POST] /api/exams/create
+module.exports.createPost = async (req, res) => {
+  if (req.body.questions && req.body.questions.length == 0)
+    delete req.body.questions;
+  if (req.body.UserExam && req.body.UserExam.length == 0)
+    delete req.body.UserExam;
+  const course = await prisma.course.findUnique({
+    where: { id: req.body.courseId },
+  });
+  if (!course) {
+    return res.status(400).json({ message: "Lỗi không tìm thấy khóa học" });
+  }
+  await prisma.exam.create({
+    data: req.body,
+  });
+  res.send("Đã tạo bài thi thành công");
+};
 
 // [POST] /api/exams/:id/createQuestion
-module.exports.createQuestion = async(req, res) => {
-    try{
-        const id = req.params.id;
+module.exports.createQuestion = async (req, res) => {
+  try {
+    const id = req.params.id;
 
-        const exam = await prisma.exam.findUnique({
-            where: {id: id}
-        });
-        if(!exam){
-            return res.status(400).json({message: "Không tìm thấy bài thi!"});
-        }
-        if(!req.body.QuestionType) req.body.QuestionType = "OBJECTIVE";
-        const questionType = req.body.QuestionType;
-        const question = await prisma.question.create({
+    const exam = await prisma.exam.findUnique({
+      where: { id: id },
+    });
+    if (!exam) {
+      return res.status(400).json({ message: "Không tìm thấy bài thi!" });
+    }
+    if (!req.body.QuestionType) req.body.QuestionType = "OBJECTIVE";
+    const questionType = req.body.QuestionType;
+    const question = await prisma.question.create({
+      data: {
+        examId: id,
+        type: questionType,
+        content: req.body.content,
+      },
+    });
+
+    if (questionType == "OBJECTIVE" || questionType == "DROPDOWN") {
+      if (!req.body.questions || req.body.questions.length <= 1) {
+        return res.status(400).json({ message: "Cần thêm câu trả lời" });
+      }
+
+      await Promise.all(
+        req.body.questions.map((item) =>
+          prisma.questionOption.create({
             data: {
-                examId: id,
-                type: questionType,
-                content: req.body.content
-            }
-        });
-        
-        
-        if(questionType == "OBJECTIVE" || questionType == "DROPDOWN"){
-            if(!req.body.questions || req.body.questions.length <= 1){
-                return res.status(400).json({message: "Cần thêm câu trả lời"});
-            }     
-
-            await Promise.all(req.body.questions.map((item) => 
-                prisma.questionOption.create({
-                    data: {
-                        questionId: question.id,
-                        content: item[0],
-                        isCorrect: item[1]
-                    }
-                })
-            ));
-        } else if(questionType == "FILL" ){
-            
-            await Promise.all(req.body.answers.map((item) => 
-                prisma.questionOption.create({
-                    data: {
-                        questionId: question.id,
-                        content: item,
-                        isCorrect: true
-                    }
-                })
-            ));
-        } else if(questionType == "MATCHING"){
-            let countOptions = 0;
-            for (const item of req.body.answers) {
-                await prisma.questionOption.create({
-                    data: {
-                        questionId: question.id,
-                        content: item[0],
-                        isCorrect: true,
-                        num: ++countOptions
-                    }
-                });
-                await prisma.questionOption.create({
-                    data: {
-                        questionId: question.id,
-                        content: item[1],
-                        isCorrect: true,
-                        num: countOptions
-                    }
-                });     
-                console.log(item[0]+ " " + item[1]);
-            }
-        } else if(questionType == "REORDERING"){
-            let countNumber = 0;
-            await Promise.all(req.body.answers.map((item) => 
-                prisma.questionOption.create({
-                    data: {
-                        questionId: question.id,
-                        content: item,
-                        isCorrect: true,
-                        num: ++countNumber
-                    }
-                })
-            ));
-        }
-        res.send("Đã tạo thành công");
-
-    } catch(error) {
-        return res.status(400).json({message: "Lỗi j đó rồi", error});
-    }
-    
-}
-
-
-// [GET] /api/exams/:id 
-module.exports.getQuestions = async(req, res) => {
-    try{
-        const id = req.params.id;
-        const exam = await prisma.exam.findUnique({
-            where: {
-                id: id,
-                deleted: false 
+              questionId: question.id,
+              content: item[0],
+              isCorrect: item[1],
             },
-            include: {
-                questions: {
-                    include: {
-                        options: true
-                    }
-                }
-            }
+          })
+        )
+      );
+    } else if (questionType == "FILL") {
+      await Promise.all(
+        req.body.answers.map((item) =>
+          prisma.questionOption.create({
+            data: {
+              questionId: question.id,
+              content: item,
+              isCorrect: true,
+            },
+          })
+        )
+      );
+    } else if (questionType == "MATCHING") {
+      let countOptions = 0;
+      for (const item of req.body.answers) {
+        await prisma.questionOption.create({
+          data: {
+            questionId: question.id,
+            content: item[0],
+            isCorrect: true,
+            num: ++countOptions,
+          },
         });
-        if(!exam){
-            return res.status(404).json({message: "Không tìm thấy bài thi"});
-        }
-        res.send(exam);
-
-    } catch(error){
-        return res.status(400).json({message: "Lỗi đéo j đó", error});
+        await prisma.questionOption.create({
+          data: {
+            questionId: question.id,
+            content: item[1],
+            isCorrect: true,
+            num: countOptions,
+          },
+        });
+        console.log(item[0] + " " + item[1]);
+      }
+    } else if (questionType == "REORDERING") {
+      let countNumber = 0;
+      await Promise.all(
+        req.body.answers.map((item) =>
+          prisma.questionOption.create({
+            data: {
+              questionId: question.id,
+              content: item,
+              isCorrect: true,
+              num: ++countNumber,
+            },
+          })
+        )
+      );
     }
-}
+    res.send("Đã tạo thành công");
+  } catch (error) {
+    return res.status(400).json({ message: "Lỗi j đó rồi", error });
+  }
+};
+
+// [GET] /api/exams/:id
+const getQuestions = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const exam = await prisma.exam.findUnique({
+      where: {
+        id: id,
+        deleted: false, // Kiểm tra trạng thái deleted
+      },
+      include: {
+        questions: {
+          include: {
+            options: true,
+          },
+        },
+      },
+    });
+    if (!exam) {
+      return res.status(404).json({ message: "Không tìm thấy bài thi" });
+    }
+    res.send(exam);
+  } catch (error) {
+    return res.status(400).json({ message: "Lỗi server", error });
+  }
+};
+module.exports.getQuestions = getQuestions;
 
 // [PATCH] /api/exams/:id?QuestionId=...
 module.exports.changeQuestionPatch = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const exam = await prisma.exam.findUnique({
-            where: {
-                id: id,
-                deleted: false 
-            }
-        });
-        if(!exam){
-            return res.status(404).json({message: "Không tìm thấy bài thi"});
-        }
-        
-        const questionId = req.query.QuestionId;
-        
-        const questionType = req.body.QuestionType || "OBJECTIVE";
-        
-        req.body.QuestionType = questionType;
-        
-        if(questionType=="DROPDOWN" || questionType == "OBJECTIVE") {
-            
-            if(!req.body.questions || req.body.questions.length <= 1){
-                return res.status(400).json({message: "Số lượng câu cần thiết là 2 trở lên"});
-            }
-        }
-        
-            
-        
-        const question = await prisma.question.findUnique({
-            where: {
-                id: questionId
-            },
-            include: {
-                options: true
-            }
-        });
-        if(!question){
-            return res.status(404).json({message: "Không tìm thấy câu hỏi"});
-        }
-        
-        await prisma.question.update({
-            where: {id: question.id},
-            data: {
-                content: req.body.content,
-                type: questionType
-            }
-        });
-        await Promise.all(question.options.map((item) => 
-            prisma.questionOption.delete({
-                where: {id: item.id}
-            })
-        ));
-       
-        if(questionType == "OBJECTIVE" || questionType == "DROPDOWN"){
-            req.body.questions.forEach(async (item) => {
-                await prisma.questionOption.create({
-                    data: {
-                        questionId: question.id,
-                        content: item[0],
-                        isCorrect: item[1] 
-                    }
-                });
-            });
-        } else if(questionType == "FILL" ){
-            
-            await Promise.all(req.body.answers.map((item) => 
-                prisma.questionOption.create({
-                    data: {
-                        questionId: question.id,
-                        content: item,
-                        isCorrect: true
-                    }
-                })
-            ));
-        } else if(questionType == "MATCHING"){
-            let countOptions = 0;
-            for (const item of req.body.answers) {
-                await prisma.questionOption.create({
-                    data: {
-                        questionId: question.id,
-                        content: item[0],
-                        isCorrect: true,
-                        num: ++countOptions
-                    }
-                });
-                await prisma.questionOption.create({
-                    data: {
-                        questionId: question.id,
-                        content: item[1],
-                        isCorrect: true,
-                        num: countOptions
-                    }
-                });     
-            }
-        } else if(questionType == "REORDERING"){
-            let countNumber = 0;
-            await Promise.all(req.body.answers.map((item) => 
-                prisma.questionOption.create({
-                    data: {
-                        questionId: question.id,
-                        content: item,
-                        isCorrect: true,
-                        num: ++countNumber
-                    }
-                })
-            ));
-        }
-        res.send("Đã thay đổi thành công");
-    } catch(error){
-        return res.status(400).json({message: "Lỗi j rồi", error});
+  try {
+    const id = req.params.id;
+    const exam = await prisma.exam.findUnique({
+      where: {
+        id: id,
+        deleted: false,
+      },
+    });
+    if (!exam) {
+      return res.status(404).json({ message: "Không tìm thấy bài thi" });
     }
-}
 
+    const questionId = req.query.QuestionId;
+
+    const questionType = req.body.QuestionType || "OBJECTIVE";
+
+    req.body.QuestionType = questionType;
+
+    if (questionType == "DROPDOWN" || questionType == "OBJECTIVE") {
+      if (!req.body.questions || req.body.questions.length <= 1) {
+        return res
+          .status(400)
+          .json({ message: "Số lượng câu cần thiết là 2 trở lên" });
+      }
+    }
+
+    const question = await prisma.question.findUnique({
+      where: {
+        id: questionId,
+      },
+      include: {
+        options: true,
+      },
+    });
+    if (!question) {
+      return res.status(404).json({ message: "Không tìm thấy câu hỏi" });
+    }
+
+    await prisma.question.update({
+      where: { id: question.id },
+      data: {
+        content: req.body.content,
+        type: questionType,
+      },
+    });
+    await Promise.all(
+      question.options.map((item) =>
+        prisma.questionOption.delete({
+          where: { id: item.id },
+        })
+      )
+    );
+
+    if (questionType == "OBJECTIVE" || questionType == "DROPDOWN") {
+      req.body.questions.forEach(async (item) => {
+        await prisma.questionOption.create({
+          data: {
+            questionId: question.id,
+            content: item[0],
+            isCorrect: item[1],
+          },
+        });
+      });
+    } else if (questionType == "FILL") {
+      await Promise.all(
+        req.body.answers.map((item) =>
+          prisma.questionOption.create({
+            data: {
+              questionId: question.id,
+              content: item,
+              isCorrect: true,
+            },
+          })
+        )
+      );
+    } else if (questionType == "MATCHING") {
+      let countOptions = 0;
+      for (const item of req.body.answers) {
+        await prisma.questionOption.create({
+          data: {
+            questionId: question.id,
+            content: item[0],
+            isCorrect: true,
+            num: ++countOptions,
+          },
+        });
+        await prisma.questionOption.create({
+          data: {
+            questionId: question.id,
+            content: item[1],
+            isCorrect: true,
+            num: countOptions,
+          },
+        });
+      }
+    } else if (questionType == "REORDERING") {
+      let countNumber = 0;
+      await Promise.all(
+        req.body.answers.map((item) =>
+          prisma.questionOption.create({
+            data: {
+              questionId: question.id,
+              content: item,
+              isCorrect: true,
+              num: ++countNumber,
+            },
+          })
+        )
+      );
+    }
+    res.send("Đã thay đổi thành công");
+  } catch (error) {
+    return res.status(400).json({ message: "Lỗi j rồi", error });
+  }
+};
