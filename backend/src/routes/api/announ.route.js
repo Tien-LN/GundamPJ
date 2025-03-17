c76162df74e9fe161b5fcbf91a37f579b1fe364d
Tien Lam <nhattienlam2005@gmail.com>
2025-03-17 10:23:35 +0700

    Update announ.route.js

--- backend/src/routes/api/announ.route.js
+++ backend/src/routes/api/announ.route.js
@@ -1,16 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/api/announ.controller");
 const validateContent = require("../../middleware/validateContent");
+const { verifyUser, checkRole } = require("../../middleware/authMiddleware");
 
-router.get("/", controller.getAllAnnouncements);
+router.get(
+  "/",
+  verifyUser,
+  checkRole(["ADMIN", "TEACHER", "STUDENT"]),
+  controller.getAllAnnouncements
+);
 
-router.get("/:id", controller.getOneAnnouncement);
+router.get(
+  "/:id",
+  verifyUser,
+  checkRole(["ADMIN", "TEACHER", "STUDENT"]),
+  controller.getOneAnnouncement
+);
 
-router.post("/create", validateContent, controller.createAnnouncement);
+router.post(
+  "/create",
+  verifyUser,
+  checkRole(["ADMIN", "TEACHER"]),
+  validateContent,
+  controller.createAnnouncement
+);
 
-router.delete("/:id", controller.deleteAnnouncement);
+router.delete(
+  "/:id",
+  verifyUser,
+  checkRole(["ADMIN"]),
+  controller.deleteAnnouncement
+);
 
-router.patch("/:id", validateContent, controller.updateAnnouncement);
+router.patch(
+  "/:id",
+  verifyUser,
+  checkRole(["ADMIN", "TEACHER"]),
+  validateContent,
+  controller.updateAnnouncement
+);
 
 module.exports = router;
