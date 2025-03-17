c0d740895a468a5605e634fd5089e14cafc59e3a
Tien Lam <nhattienlam2005@gmail.com>
2025-03-17 10:12:20 +0700

    apply checkRole

--- backend/src/routes/api/doc.route.js
+++ backend/src/routes/api/doc.route.js
@@ -1,6 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/api/doc.controller");
+const { verifyUser, checkRole } = require("../../middleware/authMiddleware");
 
-router.get("/", controller.index);
+router.get("/", verifyUser, controller.index);
+
+router.post(
+  "/create",
+  verifyUser,
+  checkRole(["TEACHER", "ADMIN"]),
+  controller.createPost
+);
+
 module.exports = router;
--- backend/src/routes/api/enrollment.route.js
+++ backend/src/routes/api/enrollment.route.js
@@ -1,21 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/api/enrollment.controller");
+const { verifyUser, checkRole } = require("../../middleware/authMiddleware");
 
-router.get("/", controller.index);
+router.get("/", verifyUser, checkRole(["ADMIN", "TEACHER"]), controller.index);
 
-router.post("/", controller.createPost);
+router.post("/", verifyUser, controller.createPost);
 
-router.delete("/:id", controller.enrollDelete);
+router.delete(
+  "/:id",
+  verifyUser,
+  checkRole(["ADMIN", "TEACHER"]),
+  controller.enrollDelete
+);
 
-router.get("/list/:id", controller.list);
+router.get(
+  "/list/:id",
+  verifyUser,
+  checkRole(["ADMIN", "TEACHER"]),
+  controller.list
+);
 
-router.get("/listApproved/:id", controller.listApproved);
+router.get(
+  "/listApproved/:id",
+  verifyUser,
+  checkRole(["ADMIN", "TEACHER"]),
+  controller.listApproved
+);
 
-router.patch("/reject/:id", controller.reject);
+router.patch(
+  "/reject/:id",
+  verifyUser,
+  checkRole(["ADMIN", "TEACHER"]),
+  controller.reject
+);
 
-router.patch("/approve/:id", controller.approve);
-
-
+router.patch(
+  "/approve/:id",
+  verifyUser,
+  checkRole(["ADMIN", "TEACHER"]),
+  controller.approve
+);
 
 module.exports = router;
