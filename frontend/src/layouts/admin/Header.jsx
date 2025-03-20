2073be50b4e31b41dd4d4805ff059f61b5595a4c
dung <quangdung21102005@gmail.com>
2025-03-20 17:11:09 +0700

    fix dateofbirth

--- frontend/src/layouts/admin/Header.jsx
+++ frontend/src/layouts/admin/Header.jsx
@@ -2,22 +2,22 @@ import { Link } from "react-router-dom";
 import CloseSider from "../../components/CloseSider";
 import Logout from "../../components/Logout";
 
-function Header(){
+function Header() {
     return (
         <>
             <div className="admin__header-containner">
                 <span className="admin__header-title">Admin</span>
-                <CloseSider/>
-                
+                <CloseSider />
+
             </div>
             <div className="my-account__containner">
                 <Link to="/admin/my-account">
-                        <i className="fa-solid fa-user"></i>
+                    <i className="fa-solid fa-user"></i>
                 </Link>
-                <Logout/>
+                <Logout />
             </div>
-            
-            
+
+
         </>
     )
 };
