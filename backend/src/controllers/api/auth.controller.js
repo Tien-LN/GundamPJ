40885c4bc627abde07c293eaf28c57c1adab2563
dung <quangdung21102005@gmail.com>
2025-03-21 22:23:01 +0700

    fix some field

--- backend/src/controllers/api/auth.controller.js
+++ backend/src/controllers/api/auth.controller.js
@@ -46,6 +46,11 @@ const login = async (req, res) => {
         id: user.id,
         email: user.email,
         role: user.role,
+        gender: user.gender,
+        phone: user.phone,
+        dateOfBirth: user.dateOfBirth,
+        address: user.address,
+        name: user.name,
         mustChangePassword: user.mustChangePassword,
       },
     });
